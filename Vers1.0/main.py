15d407707eed3b6a80342950889db865198539de
birkiy <umutberkayaltintas@gmail.com>
2020-06-03 01:13:43 +0300

    unf

--- Vers1.0/main.py
+++ Vers1.0/main.py
@@ -3,21 +3,21 @@ from Functions.Packages import *
 
 
 # print("\nGI code is running for G VCaP!\n")
-# subprocess.call ([envR + "/bin/Rscript", "--vanilla", "/home/birkiy/github/CisGraph/Vers1.0/GI2NX/GI.VCaP/GI.G.VCaP.R"])
+# subprocess.call ([envR + "/bin/Rscript", "--vanilla", f"{projectRoot}/GI2NX/GI.VCaP/GI.G.VCaP.R"])
 #
 # print("\nGI code is running for T VCaP!\n")
-# subprocess.call ([envR + "/bin/Rscript", "--vanilla", "/home/birkiy/github/CisGraph/Vers1.0/GI2NX/GI.VCaP/GI.T.VCaP.R"])
+# subprocess.call ([envR + "/bin/Rscript", "--vanilla", f"{projectRoot}/GI2NX/GI.VCaP/GI.T.VCaP.R"])
 #
 # print("\nGI code is running for C VCaP!\n")
-# subprocess.call ([envR + "/bin/Rscript", "--vanilla", "/home/birkiy/github/CisGraph/Vers1.0/GI2NX/GI.VCaP/GI.C.VCaP.R"])
+# subprocess.call ([envR + "/bin/Rscript", "--vanilla", f"{projectRoot}/GI2NX/GI.VCaP/GI.C.VCaP.R"])
 
 #
 #
 # print("\nGI code is running for G scATAC DHT!\n")
-subprocess.call ([envR , "--vanilla", home + "/GI2NX/scATAC/scATAC.DHT.R"])
+subprocess.call ([envR , "--vanilla", f"{projectRoot}/GI2NX/scATAC/scATAC.DHT.R"])
 #
 # print("\nGI code is running for G scATAC DHT!\n")
-subprocess.call ([envR , "--vanilla", home + "/GI2NX/scATAC/scATAC.ETOH.R"])
+subprocess.call ([envR , "--vanilla", f"{projectRoot}/GI2NX/scATAC/scATAC.ETOH.R"])
 
 
 # print("\nGI Levels are initializing!\n")
