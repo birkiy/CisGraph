7bf5b67a5bc0c59eca59c99f2e0a0eda2abe1263
birkiy <umutberkayaltintas@gmail.com>
2020-06-03 15:12:31 +0300

    .

--- GRAAL/GraalConvert.sh
+++ GRAAL/GraalConvert.sh
@@ -3,7 +3,7 @@
 eth=/kuacc/users/ualtintas20/github/Data/CisGraph/Vers1.0/REGAL/EtOH.edgelist
 dht=/kuacc/users/ualtintas20/github/Data/CisGraph/Vers1.0/REGAL/DHT.edgelist
 
-bash CodeAndTestData/list2leda.sh $eth >> EtOH.gw
+bash CodeAndTestData/list2leda $eth >> EtOH.gw
 bash CodeAndTestData/list2leda.sh $dht >> DHT.gw
 
-python ./GRAALRunner.py 0.8 DHT.gw EtOH.gw result
+python CodeAndTestData/GRAALRunner.py 0.8 DHT.gw EtOH.gw result
