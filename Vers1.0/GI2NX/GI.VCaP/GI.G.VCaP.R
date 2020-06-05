8a5e2486da08009d4ef01411c9a1a4b0ae1a495c
birkiy <umutberkayaltintas@gmail.com>
2020-06-05 22:57:38 +0300

    .

--- Vers1.0/GI2NX/GI.VCaP/GI.G.VCaP.R
+++ Vers1.0/GI2NX/GI.VCaP/GI.G.VCaP.R
@@ -1,5 +1,10 @@
 
 
+
+.libPaths( c( "/scratch/users/ualtintas20/apps/R/lib64", "/scratch/kuacc/apps/R/3.6.1/lib64/R/library", .libPaths() ) )
+
+
+
 suppressMessages(library(GenomicInteractions))
 
 
