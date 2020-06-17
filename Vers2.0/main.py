8fb217d2e33996fd098032300373a1ffdf75e0b2
birkiy.Ku.PC <umutberkayaltintas@gmail.com>
2020-06-18 00:51:55 +0300

    .

--- Vers2.0/main.py
+++ Vers2.0/main.py
@@ -4,7 +4,7 @@ from Functions.Packages import *
 
 # subprocess.call ([env + "/bin/Rscript", "--vanilla", "/home/birkiy/github/CisGraph/Vers2.0/GI2NX/GI.VCaP/GI.VCaP.R"])
 
-subprocess.call ([envR , "--vanilla", f"{projectRoot}/GI2NX/GI.HiC/GI.G.HiC.R"])
+subprocess.call ([envR , "--vanilla", f"{projectRoot}/GI2NX/GI.HiC/GI.HiC.R"])
 
 # import GI2NX.BuildNX.InitializeGraph
 # print("Init done!\n")
